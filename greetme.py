c00dd9ef80dd1a34ae2360a55a86b4387b0a0684
subhash2312res664 <indiansubhashkumar@gmail.com>
2024-06-12 15:08:26 +0530

    Add Greeme File

---
+++ greetme.py
@@ -0,0 +1,18 @@
+import datetime
+
+def greet():
+    global grt
+    hr = int(datetime.datetime.now().hour)
+
+    if 0 <= hr < 12:
+        grt = "Good Morning"
+    elif 12 <= hr < 15:
+        grt = "Good Afternoon"
+    elif 15 >= hr < 20:
+        grt = "Good Evening"
+    else:
+        grt = "Good Night"
+    return grt
+
+# greet()
+# print(grt)
