2142274d80a50dc2f924191cf81231d607a7c427
subhash2312res664 <indiansubhashkumar@gmail.com>
2024-06-15 12:05:23 +0530

    Add a file for Machine shutdown or restart cmd

---
+++ powermachine.py
@@ -0,0 +1,25 @@
+import os
+from spktakecmd import speak
+
+def powrstm(query):
+
+    if "shutdown the system" in query:
+        speak("Are You sure you want to shutdown")
+        shutdown = input("Do you wish to shutdown your computer? (y/n)")
+        if shutdown == "y":
+            os.system("shutdown /s /t 1")
+        elif shutdown == "no":
+            print("Ok Sir")
+            speak("Ok Sir")
+
+
+    elif "restart the system" in query:
+        speak("Are You sure you want to restart")
+        shutdown = input("Do you wish to shutdown your computer? (y/n)")
+        if shutdown == "y":
+            os.system("shutdown /r /t 1")
+        elif shutdown == "n":
+            print("Ok Sir")
+            speak("Ok Sir")
+
+
